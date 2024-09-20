606bc2965aa4944b4ca02e6193a0ab37843e90eb
nehabharti14 <nehabharti14205@gmail.com>
2024-09-20 20:20:39 +0530

    fact expand

--- factorial.js
+++ factorial.js
@@ -2,9 +2,9 @@ function fact(num) {
   let i = 0,
     fact = 1;
   while (i < num) {
-    i += 1;
-    fact *= i;
-}
-return fact;
+    i = i + 1;
+    fact = fact * i;
+  }
+  return fact;
 }
 console.log(fact(4));
